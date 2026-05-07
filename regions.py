586f79738195d965e4f500b5b04447767d650dfb
Eric Rijnboutt <erijnboutt@gmail.com>
2026-05-07 13:37:32 +0200

    Voeg RSS regio-filter webapp toe

---
+++ regions.py
@@ -0,0 +1,138 @@
+VEILIGHEIDSREGIO_KEYWORDS = {
+    "Groningen": [
+        "groningen", "veendam", "hoogezand", "sappemeer", "winschoten", "delfzijl",
+        "appingedam", "loppersum", "slochteren", "leek", "zuidhorn", "bedum",
+        "ten boer", "haren", "noordenveld", "tynaarlo", "aa en hunze", "oldambt",
+        "pekela", "stadskanaal", "bellingwedde", "vlagtwedde", "menterwolde",
+        "eemsmond", "de marne",
+    ],
+    "Fryslân": [
+        "leeuwarden", "sneek", "heerenveen", "drachten", "harlingen", "franeker",
+        "bolsward", "dokkum", "joure", "lemmer", "oosterwolde", "akkrum",
+        "friese", "friesland", "fryslân", "fryslaan", "smallingerland",
+        "tytsjerksteradiel", "achtkarspelen", "opsterland", "weststellingwerf",
+        "eaststellingwerf", "súdwest-fryslân", "de friese meren", "noardeast-fryslân",
+        "waadhoeke", "vlieland", "terschelling", "ameland", "schiermonnikoog",
+    ],
+    "Drenthe": [
+        "assen", "emmen", "hoogeveen", "meppel", "coevorden", "borger", "odoorn",
+        "drenthe", "de wolden", "westerveld", "aa en hunze", "tynaarlo",
+        "noordenveld", "midden-drenthe",
+    ],
+    "IJsselland": [
+        "zwolle", "deventer", "kampen", "hardenberg", "ommen", "raalte",
+        "olst", "wijhe", "zwartewaterland", "staphorst", "dalfsen", "steenwijkerland",
+        "hattem", "heerde", "ijsselland",
+    ],
+    "Twente": [
+        "enschede", "hengelo", "almelo", "oldenzaal", "borne", "haaksbergen",
+        "losser", "dinkelland", "tubbergen", "wierden", "hellendoorn", "rijssen",
+        "holten", "twenterand", "twente", "of twenty",
+    ],
+    "Noord- en Oost-Gelderland": [
+        "apeldoorn", "zutphen", "doetinchem", "winterswijk", "aalten", "oude ijssel",
+        "berkelland", "oost gelre", "bronckhorst", "lochem", "brummen", "voorst",
+        "epe", "heerde", "nunspeet", "elburg", "oldebroek", "hattem",
+        "montferland", "doesburg",
+    ],
+    "Gelderland-Midden": [
+        "arnhem", "nijkerk", "barneveld", "ede", "wageningen", "rhenen",
+        "renkum", "lingewaard", "overbetuwe", "scherpenzeel", "neder-betuwe",
+        "buren", "tiel", "west maas en waal", "maasdriel", "zaltbommel",
+        "culemborg", "geldermalsen", "neerijnen", "linge", "giessenlanden",
+    ],
+    "Gelderland-Zuid": [
+        "nijmegen", "wijchen", "beuningen", "druten", "groesbeek", "millingen",
+        "ubbergen", "heumen", "berg en dal", "mook", "middelaar", "gennep",
+        "grave", "cuijk", "boxmeer", "mill", "land van cuijk",
+    ],
+    "Utrecht": [
+        "utrecht", "amersfoort", "veenendaal", "zeist", "nieuwegein", "houten",
+        "ijsselstein", "de bilt", "soest", "baarn", "bunschoten", "rhenen",
+        "wijk bij duurstede", "utrechtse heuvelrug", "woerden", "de ronde venen",
+        "stichtse vecht", "lopik", "oudewater", "montfoort", "vianen", "abcoude",
+    ],
+    "Noord-Holland Noord": [
+        "alkmaar", "hoorn", "den helder", "heerhugowaard", "langedijk",
+        "enkhuizen", "medemblik", "koggenland", "opmeer", "hollands kroon",
+        "schagen", "bergen", "castricum", "heiloo", "alkmaar", "texel",
+        "drechterland", "stede broec",
+    ],
+    "Zaanstreek-Waterland": [
+        "zaandam", "zaanstad", "purmerend", "edam", "volendam", "waterland",
+        "wormerland", "beemster", "zeevang", "landsmeer",
+    ],
+    "Kennemerland": [
+        "haarlem", "haarlemmermeer", "velsen", "heemstede", "bloemendaal",
+        "zandvoort", "beverwijk", "heemskerk", "uitgeest", "castricum",
+        "schiphol",
+    ],
+    "Amsterdam-Amstelland": [
+        "amsterdam", "amstelveen", "diemen", "ouder-amstel", "aalsmeer",
+        "uithoorn",
+    ],
+    "Gooi en Vechtstreek": [
+        "hilversum", "bussum", "naarden", "huizen", "blaricum", "laren",
+        "muiden", "weesp", "wijdemeren", "gooi",
+    ],
+    "Haaglanden": [
+        "den haag", "'s-gravenhage", "delft", "rijswijk", "zoetermeer",
+        "leidschendam", "voorburg", "wassenaar", "pijnacker", "nootdorp",
+        "westland", "midden-delfland",
+    ],
+    "Hollands Midden": [
+        "leiden", "gouda", "alphen aan den rijn", "bodegraven", "reeuwijk",
+        "waddinxveen", "boskoop", "kaag en braassem", "leiderdorp", "oegstgeest",
+        "voorschoten", "zoeterwoude", "teylingen", "nieuwkoop", "krimpenerwaard",
+        "zuidplas",
+    ],
+    "Rotterdam-Rijnmond": [
+        "rotterdam", "schiedam", "vlaardingen", "maassluis", "capelle aan den ijssel",
+        "krimpen aan den ijssel", "ridderkerk", "barendrecht", "albrandswaard",
+        "spijkenisse", "nissewaard", "goeree-overflakkee", "hellevoetsluis",
+        "brielle", "westvoorne",
+    ],
+    "Zuid-Holland Zuid": [
+        "dordrecht", "papendrecht", "sliedrecht", "alblasserdam", "hendrik-ido-ambacht",
+        "zwijndrecht", "hardinxveld-giessendam", "gorinchem", "leerdam",
+        "zederik", "molenwaard", "graafstroom",
+    ],
+    "Zeeland": [
+        "middelburg", "vlissingen", "goes", "terneuzen", "hulst", "sluis",
+        "schouwen-duiveland", "tholen", "kapelle", "reimerswaal", "borsele",
+        "veere", "north sea port", "zeeland",
+    ],
+    "Midden- en West-Brabant": [
+        "breda", "tilburg", "roosendaal", "bergen op zoom", "waalwijk",
+        "oosterhout", "dongen", "geertruidenberg", "drimmelen", "zundert",
+        "etten-leur", "halderberge", "moerdijk", "loon op zand", "goirle",
+        "gilze en rijen", "alphen-chaam", "baarle-nassau",
+    ],
+    "Brabant-Noord": [
+        "'s-hertogenbosch", "den bosch", "oss", "veghel", "vught", "boxtel",
+        "haaren", "heusden", "bernheze", "maasdonk", "schijndel", "sint-michielsgestel",
+        "sint-oedenrode", "meierijstad", "land van cuijk", "grave", "mill",
+        "gennep", "cuijk", "boxmeer",
+    ],
+    "Brabant-Zuidoost": [
+        "eindhoven", "helmond", "nuenen", "son en breugel", "best", "waalre",
+        "veldhoven", "oirschot", "eersel", "deurne", "asten", "someren",
+        "gemert-bakel", "laarbeek", "geldrop-mierlo", "cranendonck", "heeze-leende",
+        "bladel", "reusel-de mierden", "bergeijk",
+    ],
+    "Limburg-Noord": [
+        "venlo", "venray", "horst aan de maas", "peel en maas", "beesel",
+        "bergen", "gennep", "mook en middelaar", "weert", "nederweert",
+        "leudal", "roermond", "echt-susteren", "maasgouw",
+    ],
+    "Zuid-Limburg": [
+        "maastricht", "heerlen", "sittard", "geleen", "kerkrade", "valkenburg",
+        "brunssum", "landgraaf", "beekdaelen", "gulpen-wittem", "meerssen",
+        "vaals", "eijsden-margraten", "stein", "beek", "simpelveld",
+        "nuth", "onderbanken", "schinnen",
+    ],
+    "Flevoland": [
+        "lelystad", "almere", "emmeloord", "dronten", "urk", "zeewolde",
+        "noordoostpolder", "flevoland",
+    ],
+}
